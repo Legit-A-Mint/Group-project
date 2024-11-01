cef5d9b62bd0c628441a7623db6f7c0b54874093
Legit-A-Mint <legit.a.minty@gmail.com>
2024-11-01 14:40:18 -0400

    classes added

---
+++ GroupJavaProject/Backpack.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Backpack here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Backpack extends Actor
+{
+    /**
+     * Act - do whatever the Backpack wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
