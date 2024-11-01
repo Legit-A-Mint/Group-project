ad833ee0398b240f15b7ab8dfec3febf59735280
vowopp <ballz869lol@gmail.com>
2024-11-01 14:44:21 -0400

    aef
    aef

--- GroupJavaProject/Player.java
+++ GroupJavaProject/Player.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.GreenfootImage;
 
 /**
  * Write a description of class Player here.
@@ -8,12 +9,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Player extends Effects
 {
-    /**
-     * Act - do whatever the Player wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-        // Add your action code here.
+    protected GreenfootImage playerImage;
+    protected int direction;
+    public Player(){
+        playerImage = new GreenfootImage("shark.png");
+        setImage(playerImage);
+        direction = 1;
+    }
+    public void act(){
+        if(direction == -1){}
+        if(Greenfoot.isKeyDown("a")){
+            direction = -1;
+        }
+        if(Greenfoot.isKeyDown("d")){
+            direction = 1;
+        }
     }
 }
