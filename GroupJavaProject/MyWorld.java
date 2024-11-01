edda5dadb7c07bc3a82d1801bfb650bde543dd62
vowopp <ballz869lol@gmail.com>
2024-11-01 14:31:21 -0400

    addeds
    stuff

--- GroupJavaProject/MyWorld.java
+++ GroupJavaProject/MyWorld.java
@@ -1,31 +1,24 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * me
  */
 public class MyWorld extends World
-{
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
-    public MyWorld()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        /**
-         * false perameter creates an unbounded world
-         */
-        super(600, 400, 1, false); 
+{   
+    private GreenfootImage img;
+    
+    public MyWorld(){  
+        //create an unbounded world 
+        super(1024, 576, 1, false);
+        img = new GreenfootImage(this.getWidth(), this.getHeight());
+        img.setColor(Color.BLUE);
+        img.fill();
+        setBackground(img);
     }
     
     public void act(){
-        /**
-         * debug System out prints out non inspectables
-         */
+        //debug System out prints out non inspectables
+        
         // System.out.print(....);
         
         
---
+++ GroupJavaProject/ScrollableWorld.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ScrollableWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ScrollableWorld extends Effects
+{
+    protected GreenfootImage worldImage;
+    protected int moveSpeed;
+    protected static final double WORLDSIZE = 1.5;
+    public ScrollableWorld(){
+        worldImage = new GreenfootImage((int)(1024 * WORLDSIZE), (int)(576 * WORLDSIZE));
+        worldImage.setColor(Color.GREEN);
+        worldImage.fill();
+        setImage(worldImage);
+        
+        //set Movespeed (varies)
+        moveSpeed = 5;
+    }
+    
+    
+    public void act()
+    {
+       if(Greenfoot.isKeyDown("w")){
+           this.setLocation(this.getX(), this.getY() -1 * moveSpeed);
+       }
+       if(Greenfoot.isKeyDown("a")){
+           this.setLocation(this.getX() -1 * moveSpeed, this.getY());
+       }
+       if(Greenfoot.isKeyDown("s")){
+           this.setLocation(this.getX(), this.getY() +1 * moveSpeed); 
+       }
+       if(Greenfoot.isKeyDown("d")){
+           this.setLocation(this.getX() +1 * moveSpeed, this.getY());
+       }
+    }
+}
