13454c60e2e676f7262e28561234c2ee460ecf13
Legit-A-Mint <legit.a.minty@gmail.com>
2024-11-01 14:14:06 -0400

    Add files via upload

---
+++ GroupJavaProject/MyWorld.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        /**
+         * false perameter creates an unbounded world
+         */
+        super(600, 400, 1, false); 
+    }
+    
+    public void act(){
+        /**
+         * debug System out prints out non inspectables
+         */
+        // System.out.print(....);
+        
+        
+    }
+}
