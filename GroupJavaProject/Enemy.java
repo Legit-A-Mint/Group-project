a17c31eb2a52259b692a3d32fd9ef98eeb3bd4ba
Legit-A-Mint <legit.a.minty@gmail.com>
2024-11-08 00:51:56 -0500

    .

--- GroupJavaProject/Enemy.java
+++ GroupJavaProject/Enemy.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
 
 /**
  * Write a description of class Enemy here.
@@ -15,12 +16,16 @@ public abstract class Enemy extends Effects
     protected int dir;
     
     private boolean removeMe;
-    private double speedX, speedY;
+    private double speedX, speedY, pushDistX, pushDistY;
+    private int[] cordsVector = new int[2];
     
     private static final int MAX_SPAWN_DISTANCE = 350;
     private static final int MIN_SPAWN_DISTANCE = 100;
     
     protected int spawnX, spawnY;
+    
+    private int repelPriority;
+    private static int repelPrioTracker = 0;
     
     protected abstract boolean checkForCollision();
     public Enemy(){
@@ -35,8 +40,12 @@ public abstract class Enemy extends Effects
         }
         removeMe = false;
         
+        cordsVector[0] = 0;
+        cordsVector[1] = 1;
         
-        
+        repelPriority = 0 + repelPrioTracker;
+        repelPrioTracker++;
+
         //enableStaticRotation();
     }
     
@@ -45,6 +54,8 @@ public abstract class Enemy extends Effects
         dir = 1;
         speedX = speed;
         speedY = speed;
+        
+        repelEnemies();
         
         lookForTarget();
         
@@ -85,16 +96,112 @@ public abstract class Enemy extends Effects
         relativeX += speedX;
         relativeY += speedY;
         
+        pushDistX += cordsVector[0];
+        pushDistY += cordsVector[1];
+        
         //Sets the location of object based on the relative position in ScrollableWorld
         //SpawnX and SpawnY handles spawn location
-        setLocation(sw.getX() + relativeX + spawnX, sw.getY() + relativeY + spawnY);
+        setLocation(sw.getX() + relativeX + spawnX + pushDistX, sw.getY() + relativeY + spawnY + pushDistY);
     }
     
-    public void spawn(){
+    public void repelEnemies() {
+        ArrayList<Enemy> enemiesTouching = (ArrayList<Enemy>)getIntersectingObjects(Enemy.class);
         
+        ArrayList<Enemy> actorsTouching = new ArrayList<Enemy>();
+        
+        ArrayList<Actor> subPriority = new ArrayList<Actor>();
+        
+        // this works, but doesn't ignore knocked down Enemies
+        //actorsTouching.addAll(pedsTouching);
+        for (Enemy e : enemiesTouching){
+            actorsTouching.add(e);
+            
+        }
+        
+        for(Enemy e : actorsTouching){
+            if(this.repelPriority > e.getRepelPriority()){
+                subPriority.add(e);
+            }
+        }
+        
+        cordsVector[0] = 0;
+        cordsVector[1] = 1;
+        
+        pushAwayFromObjects(subPriority, 2);
+        
+        
+        
+        /**
+        if(this.repelPriority > getRepelPriority()){
+            pushAwayFromObjects(subPriority, 1);
+        }
+        */
     }
     
-    public void moveIndependantly(){
+    public int[] pushAwayFromObjects(ArrayList<Actor> nearbyObjects, double minDistance) {
+        // Get the current position of this actor
+        int currentX = getX();
+        int currentY = getY();
+    
+        // Iterate through the nearby objects
+        for (Actor object : nearbyObjects) {
+            // Get the position and bounding box of the nearby object
+            int objectX = object.getX();
+            int objectY = object.getY();
+            int objectWidth = object.getImage().getWidth()/2;
+            int objectHeight = object.getImage().getHeight()/2 ;
+    
+            // Calculate the distance between this actor and the nearby object's bounding oval
+            double distance = Math.sqrt(Math.pow(currentX - objectX, 2) + Math.pow(currentY - objectY, 2));
+    
+            // Calculate the effective radii of the bounding ovals
+            double thisRadius = Math.max(getImage().getWidth() / 2.0, getImage().getHeight() / 2.0);
+            double objectRadius = Math.max(objectWidth / 2.0, objectHeight / 2.0);
+    
+            // Check if the distance is less than the sum of the radii
+            if (distance < (thisRadius + objectRadius + minDistance)) {
+                // Calculate the direction vector from this actor to the nearby object
+                int deltaX = objectX - currentX;
+                int deltaY = objectY - currentY;
+    
+                // Calculate the unit vector in the direction of the nearby object
+                double length = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+                double unitX = deltaX / length;
+                double unitY = deltaY / length;
+    
+                // Calculate the amount by which to push the nearby object
+                double pushAmount = (thisRadius + objectRadius + minDistance) - distance;
+    
+                // Update the position of the nearby object to push it away
+                
+                
+                /**
+                 * Seperate push direction based on vector of movement
+                 * This is to prevent cats from pushing eachother off the world once they begin 
+                 * walking to the boat
+                 */
+                if(objectX < currentX){
+                    cordsVector[0] = -(int)(pushAmount * unitX);
+                }else{
+                    cordsVector[0] = (int)(pushAmount * unitX);
+                }
+                
+                if(objectX < currentX){
+                    cordsVector[1] = (int)(pushAmount * unitY);
+                }else{
+                    cordsVector[1] = -(int)(pushAmount * unitY);
+                }
+                
+                
+                // object.setLocation(objectX + (int)(pushAmount * unitX), objectY + (int)(pushAmount * unitY));
+                
+            }
+        }
         
+        return cordsVector;
+    }
+    
+    public int getRepelPriority(){
+        return repelPriority;
     }
 }
