7ce8747ab16ae4d60cc3824f8fc1d09ba5b1a43d
darius-ho <150280900+darius-ho@users.noreply.github.com>
2024-11-08 14:24:38 -0500

    Reapply "Merge branch 'main' of https://github.com/Legit-A-Mint/Group-project"
    This reverts commit 6d680c40d4151d1aa5cfa81a2b5e78cac1b47dd5.

--- GroupJavaProject/Spear.java
+++ GroupJavaProject/Spear.java
@@ -6,12 +6,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Spear extends Shop
+public class Spear extends Item
 {
-    /**
-     * Act - do whatever the Harpoon wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public Spear (int x, int y)
+    {
+        itemImage = new GreenfootImage("Spear.png");
+        itemImage.scale(x,y);
+        setImage(itemImage);
+    }
     public void act()
     {
         // Add your action code here.
